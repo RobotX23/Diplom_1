025e9efe8e529745549f06fe5fd67df3b1da5421
RobotX23 <RobotX23@yandex.ru>
2024-07-23 22:49:50 +0700

    Покрыт тестами класс Ingredient

---
+++ data.py
@@ -0,0 +1,5 @@
+from ingredient_types import *
+
+KETCHUP = [INGREDIENT_TYPE_SAUCE, 'кетчуп', 1000]
+POTATO =  [INGREDIENT_TYPE_FILLING, 'картошка', 99.10]
+MUSTARD = [INGREDIENT_TYPE_SAUCE, 'горчица', 22.22]
---
+++ tests/test_ingredient.py
@@ -0,0 +1,20 @@
+from ingredient import Ingredient
+from data import *
+import pytest
+
+class TestIngredient():
+
+    @pytest.mark.parametrize('ingredient_type ,name, price', [(POTATO[0], POTATO[1], POTATO[2])])
+    def test_price(self, ingredient_type, name, price):
+        collector = Ingredient(ingredient_type=ingredient_type, name=name, price=price)
+        assert collector.get_price() == 99.10
+
+    @pytest.mark.parametrize('ingredient_type ,name, price', [(KETCHUP[0], KETCHUP[1], KETCHUP[2])])
+    def test_name(self, ingredient_type, name, price):
+        collector = Ingredient(ingredient_type=ingredient_type, name=name, price=price)
+        assert collector.get_name() == 'кетчуп'
+
+    @pytest.mark.parametrize('ingredient_type ,name, price', [(MUSTARD[0], MUSTARD[1], MUSTARD[2])])
+    def test_get_type(self, ingredient_type, name, price):
+        collector = Ingredient(ingredient_type=ingredient_type, name=name, price=price)
+        assert collector.get_type() == 'SAUCE'
